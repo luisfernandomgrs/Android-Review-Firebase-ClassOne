599a8ebb15e34db8959b031ef95ba4e1090887cc
Luis Fernando <luisfernandomgrs@gmail.com>
2025-02-05 21:03:18 -0300

    UI: First implementation to action of the login user

--- app/src/main/java/com/example/review_firebase_classone/ui/subscribe/FormSubscribe.kt
+++ app/src/main/java/com/example/review_firebase_classone/ui/subscribe/FormSubscribe.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.review_firebase_classone.R
+import com.example.review_firebase_classone.databinding.ActivityFormSubscribeBinding
 
 class FormSubscribe : AppCompatActivity() {
+    lateinit var binding : ActivityFormSubscribeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_form_subscribe)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //enableEdgeToEdge()
+        binding = ActivityFormSubscribeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
