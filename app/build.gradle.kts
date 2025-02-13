f7d1b6fecfa0a628ecc1c05b482dcb82861e5e67
Luis Fernando <luisfernandomgrs@gmail.com>
2025-02-13 19:33:11 -0300

    Added dependency from firebase-authentication and insert register of new account user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,11 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+
+    // Import the BoM for the Firebase platform
+    //implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/review_firebase_classone/ui/subscribe/FormSubscribe.kt
+++ app/src/main/java/com/example/review_firebase_classone/ui/subscribe/FormSubscribe.kt
@@ -1,5 +1,6 @@
 package com.example.review_firebase_classone.ui.subscribe
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +8,38 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.review_firebase_classone.R
 import com.example.review_firebase_classone.databinding.ActivityFormSubscribeBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormSubscribe : AppCompatActivity() {
     lateinit var binding : ActivityFormSubscribeBinding
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         binding = ActivityFormSubscribeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener { view ->
+            val edit_email = binding.editEmail.text.toString()
+            val edit_password = binding.editPassword.text.toString()
+
+            if (edit_email.isEmpty() || edit_password.isEmpty()) {
+                val snackbar = Snackbar.make(view, "It's necessary that fill all fields!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            }
+            else {
+                auth.createUserWithEmailAndPassword(edit_email, edit_password).addOnCompleteListener { authResult ->
+                    if (authResult.isSuccessful) {
+                        val snackbar = Snackbar.make(view, "User registered successfully", Snackbar.LENGTH_INDEFINITE)
+                        snackbar.setBackgroundTint(Color.BLUE)
+                        snackbar.show()
+                    }
+                }
+
+            }
+        }
     }
 }
