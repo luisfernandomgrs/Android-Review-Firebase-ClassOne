a00eb58d16b80a644fe30bf3e52548262a2f12b6
Luis Fernando <luisfernandomgrs@gmail.com>
2025-03-09 17:07:10 -0300

    Code-feature: Main activity, with logout button

--- app/src/main/java/com/example/review_firebase_classone/ui/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/review_firebase_classone/ui/MainActivity/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.example.review_firebase_classone.ui.MainActivity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.review_firebase_classone.R
+import com.example.review_firebase_classone.databinding.ActivityLoginBinding
+import com.example.review_firebase_classone.databinding.ActivityMainBinding
+import com.example.review_firebase_classone.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.fabLogoutAction.setOnClickListener { view ->
+            FirebaseAuth.getInstance().signOut()
+
+            val intentLoginActivity = Intent(this, LoginActivity::class.java)
+            startActivity(intentLoginActivity)
+
+            // In this context, is recommended to finish the current context, because you don't need more of this activity
+            finish()
         }
     }
 }
